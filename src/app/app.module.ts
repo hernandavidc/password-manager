5c4489eb11c4852a9e49ee5331912bea7908d8e0
Hernan Alvarez Caballero <hd.alvarez@uniandes.edu.co>
2024-07-27 20:44:59 -0500

    routing and tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PasswordListComponent } from './password-list/password-list.component';
+import { CreatePasswordComponent } from './create-password/create-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PasswordListComponent,
+    CreatePasswordComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
